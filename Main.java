9555885238d0648ef0b9208bd273f41c5e85e55a
Sayyad Mahammad <sayyadmahammad.byjus@aesl.in>
2023-01-15 01:27:22 +0530

    added leap year file

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
-public class Main{
-     public static void main(String[] args) {
-        System.out.println("Welcome to the world  computer ");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the world of computer ");
+        leapYear s = new leapYear();
+        System.out.println("the sum is equal to : " + s.LeapYear());
     }
 }
---
+++ leapYear.java
@@ -0,0 +1,6 @@
+public class leapYear {
+    int LeapYear() {
+        System.out.println("this is leap year class");
+        return 200 + 90;
+    }
+}
